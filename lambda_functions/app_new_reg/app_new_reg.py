36bbc8d70880dbf38f1488ed52e7dd2d3d595b81
DVS Krishna <dvskrishna@Krishna-Gadi-Mac-M1.local>
2024-07-22 15:37:37 -0500

    added magnum library

--- lambda_functions/app_new_reg/app_new_reg.py
+++ lambda_functions/app_new_reg/app_new_reg.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
 import pymysql
 import json
-import aws_serverless_wsgi
+import logging
+from mangum import Mangum
 
 app = Flask(__name__)
-api = Api(app)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 # Database connection
 def connect_db():
@@ -18,96 +20,84 @@ def connect_db():
         cursorclass=pymysql.cursors.DictCursor
     )
 
-class NewPatientRegistration(Resource):
-    def post(self):
-        connection = None
-        try:
-            data = request.get_json()
+@app.route('/app_new_reg', methods=['POST'])
+def new_patient_registration():
+    try:
+        data = request.get_json()
 
-            # Extract parent and address data
-            identificationNumber = data.get('identificationNumber')
-            identificationType = data.get('identificationType')
-            firstName = data.get('firstName')
-            lastName = data.get('lastName')
-            dateOfBirth = data.get('dateOfBirth')
-            biologicalSex = data.get('biologicalSex')
-            preferredPronouns = data.get('preferredPronouns')
-            phoneNumber = data.get('phoneNumber')
-            email = data.get('email')
-            address = data.get('address')
-            emergencyContactName = data.get('emergencyContactName')
-            emergencyContactNumber = data.get('emergencyContactNumber')
-            medicalHistory = data.get('medicalHistory')
-            drugHistory = data.get('drugHistory')
-            chiefComplaints = data.get('chiefComplaints')
+        # Extract parent and address data
+        identificationNumber = data.get('identificationNumber')
+        identificationType = data.get('identificationType')
+        firstName = data.get('firstName')
+        lastName = data.get('lastName')
+        dateOfBirth = data.get('dateOfBirth')
+        biologicalSex = data.get('biologicalSex')
+        preferredPronouns = data.get('preferredPronouns')
+        phoneNumber = data.get('phoneNumber')
+        email = data.get('email')
+        address = data.get('address')
+        emergencyContactName = data.get('emergencyContactName')
+        emergencyContactNumber = data.get('emergencyContactNumber')
+        medicalHistory = data.get('medicalHistory')
+        drugHistory = data.get('drugHistory')
+        chiefComplaints = data.get('chiefComplaints')
 
-            # Validate required fields
-            if not identificationNumber or not identificationType or not firstName or not lastName or not dateOfBirth or not biologicalSex or not preferredPronouns or not email or not phoneNumber or not address or not chiefComplaints:
-                return jsonify({'statusCode': 400, 'message': 'Missing required address fields'})
+        # Validate required fields
+        required_fields = [
+            'identificationNumber', 'identificationType', 'firstName', 'lastName',
+            'dateOfBirth', 'biologicalSex', 'preferredPronouns', 'email',
+            'phoneNumber', 'address', 'chiefComplaints'
+        ]
+        missing_fields = [field for field in required_fields if not data.get(field)]
+        if missing_fields:
+            return jsonify({'statusCode': 400, 'message': f'Missing required fields: {", ".join(missing_fields)}'}), 400
 
-            connection = connect_db()
+        connection = connect_db()
+        with connection:
             with connection.cursor() as cursor:
-                try:
-                    # Check if patient already exists
-                    cursor.execute("SELECT COUNT(*) as count FROM patient_info WHERE identificationNumber = %s", (identificationNumber,))
-                    result = cursor.fetchone()
-                    if result['count'] > 0:
-                        return jsonify({'message': 'Patient already exists'})
+                # Check if patient already exists
+                cursor.execute("SELECT COUNT(*) as count FROM patient_info WHERE identificationNumber = %s", (identificationNumber,))
+                result = cursor.fetchone()
+                if result['count'] > 0:
+                    return jsonify({'message': 'Patient already exists'}), 409
 
-                    # Insert patient's data into patient_info
-                    sql_patient_info = """
-                        INSERT INTO patient_info (
-                        identificationNumber, identificationType, firstName, lastName, dateOfBirth,  biologicalSex, preferredPronouns, phoneNumber, email, address, 
-                        emergencyContactName, emergencyContactNumber, medicalHistory, drugHistory
-                        )
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                    """
-                    cursor.execute(sql_patient_info, (identificationNumber, identificationType, firstName, lastName, dateOfBirth,  biologicalSex, preferredPronouns, phoneNumber,
-                                                      email, address, emergencyContactName, emergencyContactNumber, medicalHistory, drugHistory))
-                    connection.commit()
+                # Insert patient's data into patient_info
+                sql_patient_info = """
+                    INSERT INTO patient_info (
+                    identificationNumber, identificationType, firstName, lastName, dateOfBirth, biologicalSex, preferredPronouns, phoneNumber, email, address, 
+                    emergencyContactName, emergencyContactNumber, medicalHistory, drugHistory
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(sql_patient_info, (identificationNumber, identificationType, firstName, lastName, dateOfBirth, biologicalSex, preferredPronouns, phoneNumber,
+                                                  email, address, emergencyContactName, emergencyContactNumber, medicalHistory, drugHistory))
+                connection.commit()
 
+                # Insert data into emergency_registration
+                sql_emergency_registration = """
+                    INSERT INTO emergency_registration (
+                    identificationNumber, firstName, lastName, preferredPronouns, chiefComplaints
+                    )
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                cursor.execute(sql_emergency_registration, (identificationNumber, firstName, lastName, preferredPronouns, chiefComplaints))
+                connection.commit()
 
-                    # Insert data into emergency_registration
-                    sql_emergency_registration = """
-                        INSERT INTO emergency_registration (
-                        identificationNumber, firstName, lastName, preferredPronouns, chiefComplaints
-                        )
-                        VALUES (%s, %s, %s, %s, %s)
-                    """
-                    cursor.execute(sql_emergency_registration, (identificationNumber, firstName, lastName, preferredPronouns, chiefComplaints))
-                    connection.commit()
+                # Get the last inserted registrationId
+                registrationId = cursor.lastrowid
 
-                    # Get the last inserted registrationId
-                    registrationId = cursor.lastrowid
+                return jsonify({'message': 'Patient registered successfully', 'registrationId': registrationId}), 201
 
-                    return jsonify({'message': 'Patient registered successfully', 'registrationId': registrationId})
+    except pymysql.MySQLError as e:
+        return jsonify({"error": str(e)}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-
-                except pymysql.Error as e:
-                    if e.args[0] == 1205:
-                        # Retry logic
-                        for attempt in range(3):
-                            try:
-                                # Repeat the database operations
-                                cursor.execute(sql_patient_info, (identificationNumber, identificationType, firstName, lastName, dateOfBirth,  biologicalSex, preferredPronouns, phoneNumber,
-                                                      email, address, emergencyContactName, emergencyContactNumber, medicalHistory, drugHistory))
-                                connection.commit()
-
-                                cursor.execute(sql_emergency_registration, (identificationNumber, firstName, lastName, preferredPronouns, chiefComplaints))
-                                connection.commit()
-
-                                return jsonify({'message': 'Patient data inserted successfully'})
-                            except pymysql.Error as retry_error:
-                                continue
-                    else:
-                        return jsonify({"error": str(e)})
-        except Exception as e:
-            return jsonify({"error": str(e)})
-        finally:
-            if connection:
-                connection.close()
-
-api.add_resource(NewPatientRegistration, '/New-Patient-Registration')
+# Create a handler for AWS Lambda using Mangum
+handler = Mangum(app)
 
 def lambda_handler(event, context):
-    return aws_serverless_wsgi.handle_request(app, event, context)
+    return handler(event, context)
+
+if __name__ == "__main__":
+    app.run(debug=True)
